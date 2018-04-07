8776fbb6f398e9eda21b0fd54e17e9d9dd1fd2e4
Shane Samuel <samyyankam@gmail.com>
2018-04-08 01:39:48 +0200

    initial local

---
+++ src/main/java/com/shane/stryde/StrydeApplication.java
@@ -0,0 +1,12 @@
+package com.shane.stryde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrydeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrydeApplication.class, args);
+	}
+}
